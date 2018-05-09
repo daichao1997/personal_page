dd4faf06e87232e2611299f049ab4689a6a0643f
daichao1997 <1400011714@pku.edu.cn>
2018-05-09 22:30:09 +0800

    back to normal

--- event_register.js
+++
@@ -1,8 +0,0 @@
-var bglist = document.getElementById("bglist").childNodes;
-for(var i = 0; i < bglist.length; i++) {
-	bglist[i].onclick = choose_item;
-}
-var mylist = document.getElementById("mylist").childNodes;
-for(i = 0; i < mylist.length; i++) {
-	mylist[i].onclick = choose_item;
-}
--- sync.sh
+++
@@ -1,5 +0,0 @@
-/bin/cp -rf ./* /Users/admin/Dropbox/daichao1997.github.io
-cd /Users/admin/Dropbox/daichao1997.github.io
-git add .
-git commit -am $1
-git push
